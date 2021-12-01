9d81228f9991810b9950fa94e287f0a07b3e51aa
r4z5r4z5r4z5 <4086050+r4z5r4z5r4z5@users.noreply.github.com>
2021-12-01 20:53:47 +0300

    Delete --clear-after-build--.cmd

--- --clear-after-build--.cmd
+++
@@ -1,6 +0,0 @@
-del "*.suo" /S/Q/F/A
-del "*.csproj.user" /S/Q/F/A
-del "TonalityMarkingAndDigest-InProc.web.demo\bin\*.pdb" /Q
-del "TonalityMarkingAndDigest-InProc.web.demo\bin\TonalityMarkingAndDigest-InProc.web.demo.dll.config" /Q
-rd  "TonalityMarkingAndDigest-InProc.web.demo\obj" /S/Q
-rd  "TonalityMarkingAndDigest-InProc.web.demo\(logs)" /S/Q
